ba99f0160e420229db40a98670416f044c8df86a
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-08-21 17:49:05 +0900

    Update PrivateRoute.js

--- src/route/PrivateRoute.js
+++ src/route/PrivateRoute.js
@@ -1,9 +1,15 @@
-import React from "react";
-import ProductDetail from "../page/ProductDetail";
-import { Navigate } from "react-router-dom";
+import React from 'react';
+import ProductDetail from '../page/ProductDetail';
+import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ authenticate }) => {
-  return authenticate == true ? <ProductDetail /> : <Navigate to="/login" />;
+  const location = useLocation(); // 현재 경로를 가져옵니다.
+  
+  return authenticate === true ? (
+    <ProductDetail />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} /> // 접근하려던 경로를 state로 전달
+  );
 };
 
 export default PrivateRoute;

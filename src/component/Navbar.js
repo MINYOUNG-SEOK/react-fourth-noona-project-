98aa2771742c3cd9ad97a8632c49b824d0f19a32
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-08-21 16:19:05 +0900

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faHeart } from "@fortawesome/free-regular-svg-icons";
-import { faSearch, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 import "../component/Navbar.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState(""); // 검색어 상태 관리
   const menuList = [
     "Women",
     "Men",
@@ -27,56 +28,60 @@ const Navbar = () => {
   };
 
   const goToFavorites = () => {
-    navigate("/favorites"); // 즐겨찾기 페이지로 이동
+    navigate("/favorites");
   };
 
   const search = (event) => {
     if (event.key === "Enter") {
       let keyword = event.target.value;
+      console.log("검색어:", keyword);
       navigate(`/?q=${keyword}`);
+      setSearchTerm(""); // 검색 후 검색어 상태 초기화
     }
   };
 
   return (
-    <div>
+    <div className="navbar">
       <div className="header-icons">
-        <div className="login-button" onClick={goToLogin}>
-          <FontAwesomeIcon icon={faUser} className="header-icon" />
-          <div className="icon-label">로그인</div>
-        </div>
-        <div className="favorites-button" onClick={goToFavorites}>
-          <FontAwesomeIcon icon={faHeart} className="header-icon" />
-          <div className="icon-label">즐겨찾기</div>
-        </div>
-        <div className="cart-button">
-          <FontAwesomeIcon icon={faCartShopping} className="header-icon" />
-          <div className="icon-label">쇼핑백</div>
-        </div>
-      </div>
-
-      <div className="logo-area" onClick={goToHome}>
-        <img
-          width={120}
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMAaKiaRYNTRewPvMiE3pTI6o9LoTX1gpOtw&s"
-          alt="Logo"
-        />
-      </div>
-
-      <div className="menu-area">
-        <ul className="menu-list">
-          {menuList.map((menu, index) => (
-            <li key={index}>{menu}</li>
-          ))}
-        </ul>
         <div className="search-area">
           <FontAwesomeIcon icon={faSearch} />
           <input
             className="search-input"
             type="text"
             placeholder="검색"
-            onKeyDown={search} // 코드 정리, 확인 필요
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyDown={search}
           />
         </div>
+        <div className="login-button" onClick={goToLogin}>
+          <img src="/img/login-icon.png" alt="login" className="header-icon" />
+          <div className="icon-label">로그인</div>
+        </div>
+        <div className="favorites-button" onClick={goToFavorites}>
+          <img src="/img/like-icon.png" alt="like" className="header-icon" />
+          <div className="icon-label">즐겨찾기</div>
+        </div>
+        <div className="cart-button">
+          <img
+            src="/img/shoppingbag-icon.png"
+            alt="shoppingbag"
+            className="header-icon"
+          />
+          <div className="icon-label">쇼핑백</div>
+        </div>
+      </div>
+
+      <div className="logo-area" onClick={goToHome}>
+        <img src="/img/logo-icon.png" alt="logo" className="logo-icon" />
+      </div>
+
+      <div className="menu-area">
+        <ul className={`menu-list ${menuOpen ? "open" : ""}`}>
+          {menuList.map((menu, index) => (
+            <li key={index}>{menu}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );

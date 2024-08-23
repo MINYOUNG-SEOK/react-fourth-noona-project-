6759d9a812810e485ffcbcb332c88b366f1c8600
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-08-23 20:29:18 +0900

    Update PrivateRoute.js

--- src/route/PrivateRoute.js
+++ src/route/PrivateRoute.js
@@ -1,12 +1,11 @@
 import React from "react";
-import ProductDetail from "../page/ProductDetail";
 import { Navigate, useLocation } from "react-router-dom";
 
-const PrivateRoute = ({ authenticate }) => {
+const PrivateRoute = ({ component: Component, authenticate }) => {
   const location = useLocation();
 
   return authenticate === true ? (
-    <ProductDetail />
+    <Component />
   ) : (
     <Navigate to="/login" state={{ from: location }} />
   );

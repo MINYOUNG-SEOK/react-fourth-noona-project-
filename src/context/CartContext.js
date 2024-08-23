df7b5aa218d27f41cc3da6cf71496f73c30026f4
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-08-23 17:09:00 +0900

    Update CartContext.js

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -9,18 +9,15 @@ export const CartProvider = ({ children }) => {
 
   const addToCart = (product) => {
     setCartItems((prevItems) => {
-      // 같은 상품과 사이즈가 이미 장바구니에 있는지 확인
       const existingItemIndex = prevItems.findIndex(
         (item) => item.id === product.id && item.selectedSize === product.selectedSize
       );
 
       if (existingItemIndex !== -1) {
-        // 같은 상품이 이미 있다면 수량을 +1 증가시킴
         const updatedItems = [...prevItems];
         updatedItems[existingItemIndex].quantity += 1;
         return updatedItems;
       } else {
-        // 그렇지 않으면 새 상품을 장바구니에 추가
         return [...prevItems, { ...product, quantity: 1 }];
       }
     });
